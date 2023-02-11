ca5729ee5e2e39dcfbcaad6a953ad9dfd960a3ed
Ricky Tran <rickie.tran7@gmail.com>
2023-02-11 02:21:15 -0500

    testing commits (and removed 2 imports)

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException
 from dotenv import load_dotenv
-import time
 import os
 
 load_dotenv()
